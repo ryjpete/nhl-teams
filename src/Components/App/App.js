3f17960c491bb9da9bfb51a4b3a169b80bcc9ee0
ryjpeterson <ryan.j.peterson.80@gmail.com>
2020-08-26 10:04:38 -0500

    style and pull in different data

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import template from './App.jsx'
 
-// const clientId = 'fbd5ba45-3fbe-4b66-af78-2b3b5b'
-// const pass = 'LowStretch1005'
-
 class App extends React.Component {
   constructor(props) {
     super(props)
--- src/Components/Teams/Teams.js
+++ src/Components/Teams/Teams.js
@@ -6,7 +6,7 @@ const TeamLogo = (props) => {
   const teamName = props.teamName.replace(' ', '-')
   const teamLogo = require('./' + teamLocation.toLowerCase() + '-' + teamName.toLowerCase() + '.png')
   return (
-    <div className='logo_container bg-white p-4 rounded-circle'>
+    <div className='logo_container bg-white mt-0 mx-auto mb-3 p-4 rounded-circle'>
       <img
         src={teamLogo}
         alt={props.location} />
@@ -18,12 +18,13 @@ const Teams = ({ teamList }) => {
   return (
     <div className='d-flex flex-row flex-wrap justify-content-start'>
       {teamList.map((team) => (
-        <div key={team.id} className="card col-3 bg-dark rounded-0 text-center">
+        <div key={team.id} className="border-dark card col-2 rounded-0 text-center team">
           <div className="card-body align-items-center">
             <TeamLogo
               teamName={team.teamName}
               location={team.locationName} />
-            <h5 className="card-title m-auto">{team.name}</h5>
+            <h5 className="card-title m-auto">{team.locationName}</h5>
+            <h4 className="text-uppercase">{team.teamName}</h4>
           </div>
         </div>
       ))}
